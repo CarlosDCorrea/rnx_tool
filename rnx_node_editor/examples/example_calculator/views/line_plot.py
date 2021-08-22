7f3821764514d7f0dac1972e29d70e6ad1143320
Carlos <carlosdcorrea3@gmail.com>
2021-08-22 16:04:32 -0500

    mejorado el linechart para rnx

--- rnx_node_editor/examples/example_calculator/views/line_plot.py
+++ rnx_node_editor/examples/example_calculator/views/line_plot.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from pandas import DataFrame
 import numpy as np
-from numpy import ndarray
-
+import random
 
 def line_chart(methods_list_descriptor):
+    markers = ['.', '>', 'o']
     bottom, top = 0, 100
     for data_method in methods_list_descriptor:
-        plt.plot(np.arange(len(data_method['rnx'])), 100 * data_method['rnx'], label=f"{data_method['title']}:{round(data_method['score'] * 100, 3)}%")
+        plt.plot(np.arange(len(data_method['rnx'])), 100 * data_method['rnx'],
+                 label=f"{data_method['title']}:{round(data_method['score'] * 100, 3)}%",
+                 marker=random.choice(markers),
+                 markevery=0.1)
 
     plt.xscale('log')
     plt.ylim(bottom, top)
-    plt.legend()
+    plt.xlabel("K")
+    plt.ylabel("100RNX (K)")
+
+    plt.legend(loc='lower center', shadow=True, fontsize='x-large')
     plt.show()
